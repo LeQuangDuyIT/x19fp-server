b945df61a20814a2fee0eee32e24023e317fc302
lqduycp <lqduycp@gmail.com>
2023-11-24 21:43:51 +0700

    Config Database & Build some middlewares

--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config.js';
+import { connectToDatabase } from './config/database.js';
+import apiLoggerMiddleware from './middlewares/apiLogger.mdw.js';
 import appRouter from './routes/index.js';
+import handleErrorMiddleware from './middlewares/handleError.mdw.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
+connectToDatabase();
+
 app.use(express.json());
 app.use(cors('*'));
 
+app.use(apiLoggerMiddleware);
+
 app.use('/api/v1', appRouter);
+
+app.use(handleErrorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
