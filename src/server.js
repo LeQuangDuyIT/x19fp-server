939b3709ed3ebcc5de0fdf85a6fbfa5c7378a76c
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2024-01-22 19:14:27 +0700

    Research socketio

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { connectToDatabase } from './config/database.js';
 import apiLoggerMiddleware from './middlewares/apiLogger.mdw.js';
 import appRouter from './routes/index.js';
 import handleErrorMiddleware from './middlewares/handleError.mdw.js';
-
+import { Server } from 'socket.io';
 const app = express();
 const PORT = process.env.PORT;
 
@@ -20,6 +20,18 @@ app.use('/api/v1', appRouter);
 
 app.use(handleErrorMiddleware);
 
-app.listen(PORT, () => {
+const expressServer = app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
+});
+
+const io = new Server(expressServer, {
+  cors: ['https://x19fp-client.onrender.com', 'http://localhost:5173']
+});
+
+io.on('connection', socket => {
+  console.log(socket.id);
+  socket.on('send-answer', answer => {
+    console.log(answer);
+    socket.broadcast.emit('recevice-answer', answer);
+  });
 });
